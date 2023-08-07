e1b7ac44dde1730ee4ea9f6fad0465e59a5cdd9a
Kutay Tıstık <kutaytistik99@gmail.com>
2023-08-07 15:09:36 +0300

    07.07.2023 Commit 2

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -13,7 +13,7 @@ export class ProductAddComponent implements OnInit {
 
   productAddFrom: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,private productService:ProductService,private toastrService:ToastrService) { }
+  constructor(private formBuilder: FormBuilder, private productService: ProductService, private toastrService: ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
@@ -28,19 +28,23 @@ export class ProductAddComponent implements OnInit {
     });
   }
 
-  add(){
-    if(this.productAddFrom.valid){
-      let productModel=Object.assign({},this.productAddFrom.value);
-      this.productService.add(productModel).subscribe(response=>{
-        console.log(response);
-        this.toastrService.success(response.message,"Başarılı");
-      },responseError=>{
-        console.log(responseError.error)
-        this.toastrService.error(responseError.error)
+  add() {
+    if (this.productAddFrom.valid) {
+      let productModel = Object.assign({}, this.productAddFrom.value);
+      this.productService.add(productModel).subscribe(response => {
+
+        this.toastrService.success(response.message, "Başarılı");
+      }, responseError => {
+        if (responseError.error.Errors.length > 0) {
+          for (let i = 0; i < responseError.error.Errors.length; i++) {
+            this.toastrService.error(responseError.error.Errors[i].ErrorMessage, "Doğrulama Hatası")
+          }
+         
+        }
       });
     }
-    else{ 
-      this.toastrService.error("Formunuz Eksik","Dikkat");
+    else {
+      this.toastrService.error("Formunuz Eksik", "Dikkat");
     }
   }
 
